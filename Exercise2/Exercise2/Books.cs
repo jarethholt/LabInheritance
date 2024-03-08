f8d035bf4cbd0e809f02ec10933261fb8c3166ec
jarethholt <jareth.holt@gmail.com>
2024-03-08 22:43:18 +0100

    feat: Add GoldenEditionBook class

--- Exercise2/Exercise2/Books.cs
+++ Exercise2/Exercise2/Books.cs
@@ -65,3 +65,14 @@ public class Book
     }
     
 }
+
+
+public class GoldenEditionBook : Book
+{
+    public GoldenEditionBook(string author, string title, decimal price) : base(author, title, price) {}
+
+    protected override decimal Price
+    {
+        get => base.Price * 1.3m;
+    }
+}
