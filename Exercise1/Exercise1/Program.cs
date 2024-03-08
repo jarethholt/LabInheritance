a084462a1e4040c8054820ab419f31e8caf631aa
jarethholt <jareth.holt@gmail.com>
2024-03-08 21:44:14 +0100

    feat: Add Main program for exercise 1

--- Exercise1/Exercise1/Program.cs
+++ Exercise1/Exercise1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Exercise1;
+
+internal class Program
+{
+    static void Main()
+    {
+        string name = Console.ReadLine()!;
+        int age = int.Parse(Console.ReadLine()!);
+
+        try
+        {
+            Child child = new(name, age);
+            Console.WriteLine(child);
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
