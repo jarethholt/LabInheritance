5f0f373d0155c9654138b346d6ffc10c9e652398
jarethholt <jareth.holt@gmail.com>
2024-03-09 07:38:24 +0100

    feat: Add Main program for exercise 2

--- Exercise2/Exercise2/Program.cs
+++ Exercise2/Exercise2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Exercise2;
+
+internal class Program
+{
+    static void Main()
+    {
+        try
+        {
+            string author = Console.ReadLine()!;
+            string title = Console.ReadLine()!;
+            decimal price = decimal.Parse(Console.ReadLine()!);
+
+            Book book = new(author, title, price);
+            GoldenEditionBook goldenEditionBook = new(author, title, price);
+
+            Console.WriteLine(book);
+            Console.WriteLine();
+            Console.WriteLine(goldenEditionBook);
+        }
+        catch (ArgumentException ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+    }
+}
